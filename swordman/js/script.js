6647f02e9e82654d8c8c9c79ff7e28c71227138b
Cecep Muhammad Ridwan <88409866+ibnahmad@users.noreply.github.com>
2021-09-04 07:44:10 -0700

    Add files via upload

---
+++ swordman/js/script.js
@@ -0,0 +1,279 @@
+
+
+
+var mulai= document.getElementById('white');
+var ultimate= document.getElementById('black');
+var devensive = document.getElementById('serang');
+var ninja1 = document.getElementsByTagName('img')[0];
+var ninja2 = document.getElementsByTagName('img')[1];
+var petir1= document.getElementsByTagName('img')[2]
+var api= document.getElementsByTagName('img')[3]
+var api2= document.getElementsByTagName('img')[4]
+var petir2= document.getElementsByTagName('img')[5]
+var sword = document.getElementsByTagName('audio')[0];
+var pop = document.getElementsByTagName('audio')[1];
+var pop2 = document.getElementsByTagName('audio')[2];
+var pop3 = document.getElementsByTagName('audio')[3];
+var pop4 = document.getElementsByTagName('audio')[4];
+
+
+
+mulai.onclick = function (){  
+setTimeout(() => {
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+
+}, 1000);
+setTimeout(() => {
+sword.play()
+ninja1.setAttribute('id', 'ninja1');
+ninja2.setAttribute('id', 'ninja2');
+
+}, 2000);
+setTimeout(() => {
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+    petir1.setAttribute('id', '')
+    api.setAttribute('id', '')
+}, 3000);
+setTimeout(() => {
+    sword.play()
+    ninja1.setAttribute('id', 'ninja1');
+    ninja2.setAttribute('id', 'ninja2');
+}, 5000);
+setTimeout(() => {
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+}, 6000);
+setTimeout(() => {
+    sword.play()
+    ninja1.setAttribute('id', 'atas1');
+    ninja2.setAttribute('id', 'atas2');
+}, 7000);
+setTimeout(() => {
+    ninja1.setAttribute('id', '');
+    ninja2.setAttribute('id', '');
+}, 8000);
+setTimeout(() => {
+    sword.play()
+    ninja1.setAttribute('id', 'atas1');
+    ninja2.setAttribute('id', 'atas2');
+}, 10000);
+setTimeout(() => {
+    sword.play()
+    ninja1.setAttribute('id', 'ninja1');
+    ninja2.setAttribute('id', 'ninja2');
+}, 11000);
+setTimeout(() => {
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+}, 12000);
+setTimeout(() => {
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+}, 13000);
+setTimeout(() => {
+sword.play()
+ninja1.setAttribute('id', 'ninja1');
+ninja2.setAttribute('id', 'ninja2');
+
+},14000);
+setTimeout(() => {
+
+ninja1.setAttribute('id', 'satu');
+ninja2.setAttribute('id', 'dua');
+    
+},15000);
+    
+setTimeout(() => {
+ninja1.setAttribute('id', 'satu');
+ninja2.setAttribute('id', 'dua');
+
+},16000);
+setTimeout(() => {
+
+ninja1.setAttribute('id', 'satu');
+ninja2.setAttribute('id', 'dua');
+
+},17000);
+setTimeout(() => {
+
+ninja1.setAttribute('id', 'satu');
+ninja2.setAttribute('id', 'dua');
+
+},18000);
+setTimeout(() => {
+
+ninja1.setAttribute('id', 'atas1');
+ninja2.setAttribute('id', 'atas2');
+
+},19000);
+setTimeout(() => {
+sword.play()
+ninja1.setAttribute('id', '');
+ninja2.setAttribute('id', '');
+},20000);
+
+
+}
+
+
+jurus = ultimate.onclick = function (){
+setTimeout(() => {
+    
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+    petir1.setAttribute('id', '')
+    api.setAttribute('id', '')
+}, 1000);
+setTimeout(() => {
+    
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+    petir1.setAttribute('id', 'petir')
+    api.setAttribute('id', '')
+}, 3000);
+setTimeout(() => {
+    
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+    petir1.setAttribute('id', '')
+    api.setAttribute('id', '')
+}, 3300);
+setTimeout(() => {
+    pop.play()
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+    petir1.setAttribute('id', 'petir')
+    api.setAttribute('id', '')
+}, 3600);
+setTimeout(() => {
+    
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+    petir1.setAttribute('id', '')
+    api.setAttribute('id', '')
+}, 3900);
+setTimeout(() => {
+    
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+    petir1.setAttribute('id', 'petir')
+    api.setAttribute('id', '')
+}, 4200);
+setTimeout(() => {
+    
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+    petir1.setAttribute('id', '')
+    api.setAttribute('id', '')
+}, 4500);
+setTimeout(() => {
+    
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+    petir1.setAttribute('id', '')
+    api.setAttribute('id', '')
+}, 5000);
+setTimeout(() => {
+    pop2.play()
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+    petir1.setAttribute('id', '')
+    api.setAttribute('id', 'api')
+}, 6000);
+setTimeout(() => {
+    
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+    petir1.setAttribute('id', '')
+    api.setAttribute('id', 'api2')
+}, 7000);
+setTimeout(() => {
+    
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+    petir1.setAttribute('id', '')
+    api.setAttribute('id', 'api3')
+}, 8000);
+setTimeout(() => {
+    
+    ninja1.setAttribute('id', 'satu');
+    ninja2.setAttribute('id', 'dua');
+    petir1.setAttribute('id', '')
+    api.setAttribute('id', '')
+
+}, 8500);
+}
+
+haha = devensive.addEventListener('click', function(){
+setTimeout(() => {
+
+ninja1.setAttribute('id', 'satu');
+ninja2.setAttribute('id', 'dua');
+
+},1000);
+setTimeout(() => {
+    pop4.play()
+ninja1.setAttribute('id', 'satu');
+ninja2.setAttribute('id', 'dua');
+api2.setAttribute('id', '');
+petir2.setAttribute('id', 'transisi2');
+},2000);
+    
+setTimeout(() => {
+    pop3.play()
+ninja1.setAttribute('id', 'satu');
+ninja2.setAttribute('id', 'dua');
+api2.setAttribute('id', '');
+petir2.setAttribute('id', '');
+},5000);
+    
+setTimeout(() => {
+    
+ninja1.setAttribute('id', 'satu');
+ninja2.setAttribute('id', 'dua');
+api2.setAttribute('id', 'transisi');
+petir2.setAttribute('id', '');
+pop3.play()
+},6000);
+
+setTimeout(() => {
+ninja1.setAttribute('id', 'satu');
+ninja2.setAttribute('id', 'dua');
+api2.setAttribute('id', '');
+petir2.setAttribute('id', '');
+},19000);
+
+setTimeout(() => {
+ninja1.setAttribute('id', '');
+ninja2.setAttribute('id', '');
+api2.setAttribute('id', '');
+petir2.setAttribute('id', '');
+},20000);
+    
+
+
+
+})
+    
+
+
+
+
+
+/*document.body.addEventListener('mousemove', function(even){
+    const elem = document.getElementById("");
+   
+    var posX=even.clientX;
+    var posY=even.clientY;
+    elem.style.top = posY + "px";
+    elem.style.right = posX + "px";
+})
+document.body.addEventListener('mousemove', function(even){
+    const burung = document.getElementById("");
+    var posX=even.clientX;
+    var posY=even.clientY;
+    burung.style.top = posY + "px";
+    burung.style.left = posX + "px";
+})*/
